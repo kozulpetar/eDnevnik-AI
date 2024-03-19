4b6f39e1e213cbef2afbaed357f4cae55ddb4337
hljubic <hrvoje.ljubic@gmail.com>
2024-03-19 10:22:34 +0100

    CORS

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import cors from 'cors'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(VueAxios, axios)
 app.use(router)
+app.use(cors())
 
 app.mount('#app')
